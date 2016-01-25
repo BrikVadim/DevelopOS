d31945b93f1aa60a56584dc7a20cdbfa51a3ae83
Vadim Brik <Brik99brik@gmail.com>
2016-01-25 14:08:18 +0600

    Update types.h
    Added new types (signed)

--- include/types.h
+++ include/types.h
@@ -12,13 +12,38 @@
 
 #define NULL 0
 
-typedef unsigned long size_t;
-typedef unsigned long ulong;
+typedef unsigned long       size_t;
+typedef unsigned long       ulong;
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned int        u32;
+typedef unsigned long long  u64;
+
+typedef unsigned char       u8int;
+typedef unsigned short      u16int;
+typedef unsigned int        u32int;
+typedef unsigned long long  u64int;
+
+typedef unsigned char       uint8;
+typedef unsigned short      uint16;
+typedef unsigned int        uint32;
+typedef unsigned long long  uint64;
+
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed int          s32;
+typedef signed long long    s64;
+
+typedef signed char         s8int;
+typedef signed short        s16int;
+typedef signed int          s32int;
+typedef signed long long    s64int;
+
+typedef signed char         sint8;
+typedef signed short        sint16;
+typedef signed int          sint32;
+typedef signed long long    sint64;
 
 #endif
 
